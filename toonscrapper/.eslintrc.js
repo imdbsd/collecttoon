b5053e42ad83814cafe0a38fe30d9c54ac8ef6e1
surya darma <budi.surya@kumparan.com>
2020-12-05 15:09:20 +0800

    add eslint prettier rule

--- toonscrapper/.eslintrc.js
+++ toonscrapper/.eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    ],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    ],
-};
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+}
